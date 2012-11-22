896715278bd0f126f599cd0384744cd94a65a668
Mike Walker <miwalker@unjeb-mbp.(none)>
2012-11-21 17:34:44 -0800

    lets see if this works

---
+++ content/foo.md
@@ -0,0 +1,6 @@
+Date: 2012-11-21
+Title: Hello world
+Category: Blog
+Tags: blogging, test
+
+I like turtles
---
+++ pelicanconf.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u"Mike"
+ARTICLE_SAVE_AS = '/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+ARTICLE_URL = '/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+SITENAME = u"noisufnoc.com"
+SITEURL = ''
+DEFAULT_CATEGORY = 'Blog'
+DEFAULT_LANG = 'en'
+DELETE_OUTPUT_DIRECTORY = True
+GITHUB_URL = 'https://github.com/noisufnoc/noisufnoc.github.com'
+TWITTER_USERNAME = 'noisufnoc'
+
+TIMEZONE = 'Europe/Paris'
+
+
+# Blogroll
+LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+          ('Python.org', 'http://python.org'),
+          ('Jinja2', 'http://jinja.pocoo.org'),
+          ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+
+#SOCIAL = (
+#    ('aclark4life', 'http://twitter.com/aclark4life'),
+#    ('ACLARKNET', 'http://twitter.com/ACLARKNET'),
+#    ('GitHub', 'http://github.com/aclark4life'),
+#    ('Gittip', 'https://www.gittip.com/aclark4life'),
+#    ('PythonPackages', 'https://pythonpackages.com/user/aclark4life'),
+#    ('atom feed (Django)', 'http://blog.aclark.net/feeds/Django.atom.xml'),
+#    ('atom feed (Mozilla)', 'http://blog.aclark.net/feeds/Mozilla.atom.xml'),
+#    ('atom feed (Plone)', 'http://blog.aclark.net/feeds/Plone.atom.xml'),
+#    ('atom feed (Python)', 'http://blog.aclark.net/feeds/Python.atom.xml'),
+#)
